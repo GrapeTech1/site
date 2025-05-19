c1450597f730212f91fa325b770d2cdc72270b3d
Camadu <mariagferreira2312@gmail.com>
2025-05-19 17:11:21 -0300

    Responsividade

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,32 @@ faqs.forEach(faq => {
         faq.classList.toggle("active");
     });
 });
+
+class MobileNavBar {
+   constructor(mobileMenu, navList, nav) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.nav = document.querySelector(nav);
+    this.activeClass =  "active";
+   } 
+
+   addClickEvent() {
+    this.mobileMenu.addEventListener("click", () => console.log
+   ("Hey motherfuck"));
+   }
+
+   init() {
+    if (this.mobileMenu) {
+        this.addClickEvent();
+    }
+    return this;
+   }
+}
+
+const MobileNavBar = new MobileNavBar (
+    ".mobile-menu",
+    "nav",
+    "nav li",
+
+);
+MobileNavBar.init();
