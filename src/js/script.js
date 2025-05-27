e912ebbb37a9a1012e8c1038a02aedce95e5a7a9
Camadu <mariagferreira2312@gmail.com>
2025-05-27 17:27:42 -0300

    responsividade

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,9 @@ function backTop()
         behavior: 'smooth'
     })
 }
+
+const hamburger = document.querySelector(".hamburguer");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => 
+nav.classList.toggle("active"));
