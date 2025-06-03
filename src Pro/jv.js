9d326feb6b6fd2a11d00ee3575a83d6867f54f58
Camadu <mariagferreira2312@gmail.com>
2025-06-03 16:41:32 -0300

    adicionando o menu hamburguer na tela Projetos

--- src Pro/jv.js
+++ src Pro/jv.js
@@ -10,3 +10,9 @@ function backTop()
         behavior: 'smooth'
     })
 }
+
+const hamburger = document.querySelector(".hamburguer");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => 
+nav.classList.toggle("active"));
